3e674c561397b14f708bddeb8b41e70307d4eda1
David Robert <contact@david-robert.fr>
2024-07-02 14:11:16 +0200

    constructor

--- Order.php
+++ Order.php
@@ -4,6 +4,10 @@ class Order {
 	public $status;
 
 
+	function __construct() {
+		$this->status = "en cours";
+	}
+
 	public function pay() {
 		$this->status = "payé";
 	}
@@ -13,13 +17,15 @@ class Order {
 		$this->status = "annulé";
 	}
 
+
 }
 
 
+// quand je veux créer une nouvelle commande
+// j'instancie (je créé un objet) la classe Order grâce au mot clé new
+// Quand je créé un objet de type order avec le mot clé new
+// la fonction constructor est automatiquement appelée
+// permettant (si besoin) d'initialiser la commande avec des propriétés par défaut
 $order = new Order();
+var_dump($order?status);
 
-$order->pay();
-$order->cancel();
-
-
-var_dump($order->status);
