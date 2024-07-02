ecac92246e0c83ebbd2785e79339e95da9f29968
David Robert <contact@david-robert.fr>
2024-07-02 15:18:40 +0200

    method add product Order

--- Order.php
+++ Order.php
@@ -1,11 +1,16 @@
 <?php
 
 class Order {
-	public $status;
+	private $status;
 
+	private $totalPrice;
+
+	private $productQty;
 
 	function __construct() {
 		$this->status = "en cours";
+		$this->totalPrice = 0;
+		$this->productQty = 0;
 	}
 
 	public function pay() {
@@ -17,15 +22,28 @@ class Order {
 		$this->status = "annulé";
 	}
 
+	public function addProduct() {
+		$this->productQty = $this->productQty + 1;
+		$this->totalPrice = $this->totalPrice + 10;
+	}
 
 }
-
 
 // quand je veux créer une nouvelle commande
 // j'instancie (je créé un objet) la classe Order grâce au mot clé new
 // Quand je créé un objet de type order avec le mot clé new
 // la fonction constructor est automatiquement appelée
 // permettant (si besoin) d'initialiser la commande avec des propriétés par défaut
-$order = new Order();
-var_dump($order?status);
+
+
+
+var_dump($order);
+
+$order2 = new Order();
+$order2->addProduct();
+$order2->pay();
+
+
+
+
 
